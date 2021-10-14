1f3e9c05339cbe235ce2424cfdb13294e991acee
HugClaisse <freehugh92@gmail.com>
2021-10-14 15:48:10 +0200

    btc

--- lib/Dark_Trader.rb
+++ lib/Dark_Trader.rb
@@ -6,12 +6,12 @@ def crypto_scrapper
     page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/"))
     puts page.class # => utilisation de la gem nokogiri:sur le site des cryptos
 
-    name_currency = page.xpath(//*[@id="__next"]/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[1]/td[2]).find_all
+    name_currency = page.xpath('//*[@id="__next"]/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[1]/td[2]').find_all  
     name_currency_array = name_currency.collect(&:text)
-    price_currency = page.xpath(//*[@id="__next"]/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[1]/td[5]).find_all
+    price_currency = page.xpath("//*[@id='__next']/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[1]/td[5]").find_all
     price_currency_array = price_currency.collect(&:text)
     price_currency_array.map!{|number| number.delete("$").to_f}
-    final_hash = Hash[name_currency_array.zip(price_array)]
+    final_hash = Hash[name_currency_array.zip(price_currency_array)]
     
     Hash.class_eval do
         def split_into(divisions)
@@ -26,3 +26,7 @@ def crypto_scrapper
      end
      puts final_hash.split_into(name_currency_array.size)   
     end
+
+    crypto_scrapper
+
+
--- spec/Dark_Trader_spec.rb
+++ spec/Dark_Trader_spec.rb
@@ -5,9 +5,8 @@ require_relative '../lib/Dark_Trader'
 describe "the Dark_Trader" do
     it "give the name of the currency" do
         #trouve le nom de la crypto
-        expect(name_currency)).to eq("BTC")
-        expect(name_currency)).to eq("BNB")
+        expect(name_currency).to eq("BTCBitcoin")
 
     end
 end
-end
+
